5080d1c5ee77992df22987d5451e9a8d3d3c4fd9
aaronmac51 <aaron.mcgeehon@gmail.com>
2019-11-23 13:36:02 -0700

    create linux vm

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+	version	 			= "-> 2.0"
+	region				= "us-west-2"
+	shared_credentials_file 	= "/home/aaronmac5.aws/credentials"
+	profile 			= "terraform"
+}
+
+resource "aws_instance" "web" {
+	ami				= ""
+	instance_type			= "t2.micro"
+	tags = {
+		name = linuxtest"
+	}
+	key_name			= "yolo"
+}
